8544539c43a43c7b86b7918e8892f5b6aefb5d0e
DisterDE <disterko@gmail.com>
2024-10-27 22:32:55 +0100

    Refactor StatisticsServiceImplTest to use mock CacheImpl
    Replaced in-memory logic with cache mocks to improve test reliability. Ensured proper interaction by verifying cache operations such as save, getBy, and getEntries.

--- src/test/kotlin/com/github/disterde/uselessfactsapi/service/StatisticsServiceImplTest.kt
+++ src/test/kotlin/com/github/disterde/uselessfactsapi/service/StatisticsServiceImplTest.kt
@@ -1,38 +1,49 @@
 package com.github.disterde.uselessfactsapi.service
 
+import com.github.disterde.uselessfactsapi.component.cache.CacheImpl
 import com.github.disterde.uselessfactsapi.domain.StatisticsResponse
-import kotlin.test.*
+import io.mockk.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
 
 class StatisticsServiceImplTest {
 
-    private lateinit var statisticsService: StatisticsServiceImpl
-
-    @BeforeTest
-    fun setup() {
-        statisticsService = StatisticsServiceImpl()
-    }
+    private val cache = mockk<CacheImpl<String, AtomicInteger>>()
+    private val statisticsService = StatisticsServiceImpl(cache)
 
     @Test
     fun `should save a new url`() {
+        every { cache.save(URL, any<AtomicInteger>()) } just runs
+
         statisticsService.save(URL)
 
-        val result = statisticsService.getStatistics()
-
-        assertTrue { result.size == 1 }
-        assertContains(result, RESPONSE)
+        verify { cache.save(URL, any<AtomicInteger>()) }
     }
 
     @Test
     fun `should increment access count correctly`() {
-        statisticsService.save(URL)
+        every { cache.getBy(URL) } returns VALUE_MOCK
+
         statisticsService.incrementAccessCount(URL)
 
-        val result = statisticsService.getStatistics().first()
-        assertEquals(1, result.accessCount)
+        verify { VALUE_MOCK.incrementAndGet() }
+    }
+
+    @Test
+    fun `should return all entries from cache`() {
+        every { cache.getEntries() } returns CACHE
+
+        val result = statisticsService.getStatistics()
+
+        verify { cache.getEntries() }
+        assertContentEquals(RESPONSES, result)
     }
 
     companion object {
         private const val URL = "test"
-        private val RESPONSE = StatisticsResponse(URL, 0)
+        private val CACHE = mapOf(URL to AtomicInteger(0))
+        private val RESPONSES = listOf(StatisticsResponse(URL, 0))
+        private val VALUE_MOCK = mockk<AtomicInteger>(relaxed = true)
     }
 }
