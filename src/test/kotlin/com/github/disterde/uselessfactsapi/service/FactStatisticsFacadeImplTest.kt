a6dff1f9f9ff28b112bf6424471b6df03522be6d
DisterDE <disterko@gmail.com>
2024-10-27 22:58:26 +0100

    Add unit tests for FactStatisticsFacadeImpl
    Implemented tests for verifying the interactions between FactStatisticsFacadeImpl, UrlShortener, FactService, and StatisticsService. These tests ensure that the methods for getting random facts, retrieving facts by URL, and fetching cached facts operate correctly and interact with the dependencies as expected.

---
+++ src/test/kotlin/com/github/disterde/uselessfactsapi/service/FactStatisticsFacadeImplTest.kt
@@ -0,0 +1,61 @@
+package com.github.disterde.uselessfactsapi.service
+
+import com.github.disterde.uselessfactsapi.component.shortener.UrlShortener
+import com.github.disterde.uselessfactsapi.domain.Fact
+import com.github.disterde.uselessfactsapi.domain.ShortenedUrlFact
+import io.mockk.*
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FactStatisticsFacadeImplTest {
+
+    private val shortener = mockk<UrlShortener>()
+    private val factService = mockk<FactService>()
+    private val statisticsService = mockk<StatisticsService>()
+    private val sut = FactStatisticsFacadeImpl(shortener, factService, statisticsService)
+
+    @Test
+    fun `should call all services correctly to get a random fact`() = runTest {
+        every { shortener.getShortUrl() } returns URL
+        coEvery { factService.getRandomFact(URL) } returns FACT
+        every { statisticsService.save(URL) } just runs
+
+        val fact = sut.getRandomFact()
+
+        assertEquals(SHORTENED_URL_FACT, fact)
+        verify { shortener.getShortUrl() }
+        coVerify { factService.getRandomFact(URL) }
+        verify { statisticsService.save(URL) }
+    }
+
+    @Test
+    fun `should call fact service and increment access count by short url`() = runTest {
+        every { factService.getFactBy(URL) } returns FACT
+        every { statisticsService.incrementAccessCount(URL) } just runs
+
+        val result = sut.getFactBy(URL)
+
+        assertEquals(FACT, result)
+        verify { factService.getFactBy(URL) }
+        verify { statisticsService.incrementAccessCount(URL) }
+    }
+
+    @Test
+    fun `should return all facts and wrap them in response`() {
+        every { factService.getCachedFacts() } returns CACHED_FACTS
+
+        val result = sut.getCachedFacts()
+
+        assertEquals(result, CACHED_FACT_RESPONSE)
+    }
+
+    companion object {
+        private const val URL = "test"
+        private val FACT = Fact("123", "456")
+        private val FACT_RESPONSE = FACT.toResponse()
+        private val SHORTENED_URL_FACT = ShortenedUrlFact("123", URL)
+        private val CACHED_FACTS = listOf(FACT)
+        private val CACHED_FACT_RESPONSE = listOf(FACT_RESPONSE)
+    }
+}
